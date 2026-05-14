11bf552f3587fc3431017071e601650a3b838715
Santiago Alvarez Gutierrez <santiago.alvarez.gutierrez@correounivalle.edu.co>
2026-05-14 11:52:20 -0500

    Initial microservices architecture

---
+++ inventory-service/src/test/java/com/axiserp/inventory/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axiserp.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
